fa36fdf37dd0bd482ccbc6986925e0c74f94b858
Rikard Johansson <Rikard.com@hotmail.com>
2022-11-25 22:04:54 +0100

    made it better

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.cookie = "Set-Cookie: promo_shown=1; SameSite=Lax";
+
 // Key: 0388acbbb468f9fe47a5ad8e4a8bc184
 // API: https://api.openweathermap.org/data/2.5/weather?q=varberg&appid=0388acbbb468f9fe47a5ad8e4a8bc184
 
@@ -6,15 +8,29 @@ const currentTemp = document.getElementById("current-temp");
 const cardContainer = document.getElementById("card-container");
 const displayTime = document.getElementById("current-time");
 const displayImg = document.getElementById("card-img");
+const displayWeatherIcon = document.getElementById("weather-icon");
+const displayInfo = document.getElementById("info");
 // Searchbox declarations
 const searchInput = document.getElementById("search-input");
 const listWrapper = document.getElementById("list-container");
+// interval stop
+let interval;
+// result box
 let results = [];
 // TODO | Functions here
 const showErrorMessage = () => {
     searchInput.placeholder = "Cannot find city location";
 };
 
+const getTimeFromTimeZone = (timezone) => {
+    let time = new Date(new Date().getTime() + 1 * timezone * 1000);
+    let hours = time.getUTCHours();
+    let minutes = time.getMinutes();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    cardContainer.classList.add(`sky-gradient-${hours}`);
+    displayTime.innerHTML = `${hours}:${minutes}`;
+};
 //! Fetch weather data
 const getWeather = async (cityName) => {
     const response = await fetch(
@@ -25,20 +41,22 @@ const getWeather = async (cityName) => {
     console.log(data);
     currentCity.innerHTML = data.name;
     currentTemp.innerHTML = Math.floor(data.main.temp) + "°c";
+    // displayar väder iconen
+    displayWeatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    // displays info
+    cardContainer.className = "card-container";
+    displayInfo.innerHTML = data.weather[0].description;
     // kollar värmen
     if (data.main.temp > 20) displayImg.src = "/img/hot.png";
     else if (data.main.temp < 5) displayImg.src = "/img/cold.png";
     else displayImg.src = "/img/normal.png";
 
     // Hämtar tidszon
-    const time = new Date(new Date().getTime() + 1 * data.timezone * 1000);
-    let hours = time.getUTCHours();
-    let minutes = time.getMinutes();
-    if (hours < 10) hours = "0" + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    console.log(hours);
-    cardContainer.classList.add(`sky-gradient-${hours}`);
-    displayTime.innerHTML = `${hours}:${minutes}`;
+    getTimeFromTimeZone(data.timezone);
+    interval = setInterval(() => {
+        getTimeFromTimeZone(data.timezone);
+        console.log("count");
+    }, 1000);
 
     //
 };
@@ -49,7 +67,7 @@ const getCityList = async (search) => {
         .then((json) => {
             listWrapper.innerHTML = "";
             for (let i = 0; i < json.length; i++) {
-                if (results.length > 3) break;
+                if (results.length > 5) break;
                 if (json[i].name.toLowerCase().indexOf(search) !== -1)
                     results.push(json[i].name);
             }
@@ -70,6 +88,8 @@ searchInput.addEventListener("keyup", () => {
 });
 listWrapper.addEventListener("click", (e) => {
     getWeather(e.target.innerHTML);
+    clearInterval(interval);
+    searchInput.value = "";
     listWrapper.innerHTML = "";
 });
 
